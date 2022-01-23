fdefc35b5524b224e3fb7fc33708b8d913b4f0c2
mohammad-moussalli <mohammadmoussalli@gmail.com>
2022-01-23 20:04:31 +0200

    Add Rules Boolean - Stop the game when you win/lose to repeat from the start

--- game.js
+++ game.js
@@ -1,8 +1,9 @@
 window.onload = function(){
 
     var score = 0;
-    
+    var rules = false;
     function reset(){
+        rules = true;
         score = 0;
         var border_color = document.getElementsByClassName("boundary");
         for (var i = 0; i < border_color.length; i++){
@@ -14,6 +15,7 @@ window.onload = function(){
     document.getElementById("start").addEventListener("click", reset);
 
     function playAgain(){
+        rules = true;
         document.getElementById("status").innerHTML= "Begin by moving your mouse over the <q>S</q>, Your score is: " + score +".";
         var border_color = document.getElementsByClassName("boundary");
         for (var i = 0; i < border_color.length; i++){
@@ -23,21 +25,27 @@ window.onload = function(){
     document.getElementById("start").addEventListener("mouseover", playAgain);
 
     function youWin(){
-        score += 5;
-        document.getElementById("status").innerHTML= "You won, Your score is: " + score + ".";
-        var border_color = document.getElementsByClassName("boundary");
-        for (var i = 0; i < border_color.length; i++){
-            border_color[i].style.backgroundColor = "green";
+        if (rules == true){
+            score += 5;
+            document.getElementById("status").innerHTML= "You won, Your score is: " + score + ".";
+            var border_color = document.getElementsByClassName("boundary");
+            for (var i = 0; i < border_color.length; i++){
+                border_color[i].style.backgroundColor = "green";
+            }
         }
+        rules = false;
     }
     document.getElementById("end").addEventListener("mouseover", youWin);
 
     function youLose(){
-        score -= 10;
-        document.getElementById("status").innerHTML= "You lose, Your score is: " + score + ".";
-        var border_color = document.getElementsByClassName("boundary");
-        for (var i = 0; i < border_color.length; i++){
-            border_color[i].style.backgroundColor = "#ff8888";
+        if (rules == true){
+            score -= 10;
+            document.getElementById("status").innerHTML= "You lose, Your score is: " + score + ".";
+            var border_color = document.getElementsByClassName("boundary");
+            for (var i = 0; i < border_color.length; i++){
+                border_color[i].style.backgroundColor = "#ff8888";
+            }
+        rules = false;
         }
          
     }
