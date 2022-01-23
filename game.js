8f870f413c852c04c1a36aa4139d3bbd2eb627b1
mohammad-moussalli <mohammadmoussalli@gmail.com>
2022-01-23 18:38:29 +0200

    Add Scoring System

--- game.js
+++ game.js
@@ -1,25 +1,30 @@
 window.onload = function(){
+
+    var score = 0;
     
     function reset(){
-        scoringSystem() = 0;
+        score = 0;
         var border_color = document.getElementsByClassName("boundary");
         for (var i = 0; i < border_color.length; i++){
             border_color[i].style.backgroundColor = "#eeeeee";
         }  
+        document.getElementById("status").innerHTML= "Begin by moving your mouse over the <q>S</q>.";
+
     }
     document.getElementById("start").addEventListener("click", reset);
 
     function playAgain(){
-        document.getElementById("status").innerHTML= "Begin by moving your mouse over the <q>S</q>."
+        document.getElementById("status").innerHTML= "Begin by moving your mouse over the <q>S</q>, Your score is: " + score +".";
         var border_color = document.getElementsByClassName("boundary");
         for (var i = 0; i < border_color.length; i++){
             border_color[i].style.backgroundColor = "#eeeeee";
-        } 
+        }
     }
     document.getElementById("start").addEventListener("mouseover", playAgain);
 
     function youWin(){
-        document.getElementById("status").innerHTML= "You won"
+        score += 5;
+        document.getElementById("status").innerHTML= "You won, Your score is: " + score + ".";
         var border_color = document.getElementsByClassName("boundary");
         for (var i = 0; i < border_color.length; i++){
             border_color[i].style.backgroundColor = "green";
@@ -28,16 +33,17 @@ window.onload = function(){
     document.getElementById("end").addEventListener("mouseover", youWin);
 
     function youLose(){
-        document.getElementById("status").innerHTML= "You lose"
+        score -= 10;
+        document.getElementById("status").innerHTML= "You lose, Your score is: " + score + ".";
         var border_color = document.getElementsByClassName("boundary");
         for (var i = 0; i < border_color.length; i++){
             border_color[i].style.backgroundColor = "#ff8888";
-        }    
+        }
+         
     }
     var boundaries = document.getElementsByClassName("boundary");
     for (var j = 0; j < boundaries.length; j++){
         boundaries[j].addEventListener("mouseover", youLose);
     }
-
 }
     
